7dddbc042e48e84a7c868eb3cbc7e1f90340ab32
yashwanthi28 <71919596+yashwanthi28@users.noreply.github.com>
2021-11-26 15:35:14 +0530

    Add files via upload

---
+++ greatestofthree.java
@@ -0,0 +1,17 @@
+package project1;
+
+public class greatestofthree {
+	public static void main(String args[]) {
+	int a=15;
+	int b=25;
+	int c=35;
+	if(a>b && a>c) {
+		System.out.println(a+"greatest number");
+	}else if(b>c && b>1){
+		System.out.println(b+"greatest number");
+	}else
+		System.out.println(c+"is great");
+	}
+		
+	}
+		
