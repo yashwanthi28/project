03b5e748df746d233d6d11b02437adebcc36f619
yashwanthi28 <71919596+yashwanthi28@users.noreply.github.com>
2021-12-17 10:20:05 +0530

    Add files via upload

---
+++ greatestamong3.java
@@ -0,0 +1,20 @@
+package project4;
+
+public class greatestamong3 {
+
+	public static void main(String[] args) {
+		int a=20,b=30,c=50;
+		if(a>b && a>c)
+		{
+			System.out.print("a");
+					
+		}else if (b>c && b>a)
+		{
+			System.out.print("b");
+		}else
+		{
+			System.out.print("c");
+		}
+	}
+}
+		
