dbcc9e7cb0de9514c9ce0251b176ed8d324f6804
yashwanthi28 <71919596+yashwanthi28@users.noreply.github.com>
2021-12-17 10:21:13 +0530

    Add files via upload

---
+++ triangle.java
@@ -0,0 +1,15 @@
+package project5;
+
+public class triangle {
+	public static void main(String[] args) {
+		int a=10,b=10,c=10;
+		if((a+b)>c && (b+c)>a && (c+a)>b)
+		{
+			System.out.print("it is a triangle");
+			
+		}else
+			System.out.print("it is not a triangle" );
+	}
+}
+
+
