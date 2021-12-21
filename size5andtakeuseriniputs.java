fea5ba0ac7bba5281477fc1fb6f2724e03d99a35
yashwanthi28 <71919596+yashwanthi28@users.noreply.github.com>
2021-12-21 13:28:05 +0530

    Add files via upload

---
+++ size5andtakeuseriniputs.java
@@ -0,0 +1,13 @@
+package project13;
+import java.util.Scanner;
+public class size5andtakeuseriniputs {
+	    public static void main(String[] args) {
+	        int mark[]=new int[5];
+	        Scanner sc = new Scanner(System.in);
+	        for(int i=0;i<5;i++){
+	            mark[i]= sc.nextInt();
+	        }
+	    }
+	}
+
+
