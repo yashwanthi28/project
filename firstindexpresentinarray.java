8257f172a535a320363031ec376caffefa145e30
yashwanthi28 <71919596+yashwanthi28@users.noreply.github.com>
2021-12-24 17:02:26 +0530

    Add files via upload

---
+++ firstindexpresentinarray.java
@@ -0,0 +1,33 @@
+package project26;
+import java.util.Scanner;
+public class firstindexpresentinarray {
+	    public static void main(String[] args) {
+	        Scanner sc = new Scanner(System.in);
+	        System.out.println("enter the size of an array");
+	        int a = sc.nextInt();
+	        int num[]=new int[a];
+	        System.out.println("enter the values");
+	        for(int j=0;j<a;j++){
+	            num[j]= sc.nextInt();
+	        }
+	        System.out.println("enter the number you want to find");
+	        int n = sc.nextInt();
+	        boolean ispresent=false;
+	        int pos=-1;
+	        for (int i = 0; i < num.length; i++) {
+	            if(num[i]==n){
+	                ispresent=true;
+	                pos=i;
+	                break;
+	            }
+
+	        }
+	        if (ispresent){
+	            System.out.println("number is prresnt at "+pos+"  index of array");
+	        }
+	        else{
+	            System.out.println("number is not present");
+	        }
+	    }
+}
+	
