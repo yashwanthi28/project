e07b1ce15683527878f9700f6049234484078fd3
yashwanthi28 <71919596+yashwanthi28@users.noreply.github.com>
2021-12-28 17:43:15 +0530

    Add files via upload

---
+++ fahrenheittocelsius.java
@@ -0,0 +1,13 @@
+package project35;
+
+public class fahrenheittocelsius {
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			float Fahrenheit;
+			float Celsius;
+			Fahrenheit = 10;
+			Celsius = ((Fahrenheit-32)*5)/9;
+			System.out.println("Temperature in celsius is: "+Celsius);
+		}
+	}
+
