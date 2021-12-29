73fd1782ce6f57c8be1fc235be29c58c967392fb
yashwanthi28 <71919596+yashwanthi28@users.noreply.github.com>
2021-12-29 17:50:00 +0530

    Add files via upload

---
+++ positiveornegative.java
@@ -0,0 +1,25 @@
+package project40;
+import java.util.Scanner;
+public class positiveornegative {
+    public static String neg_or_pos(int n){
+        String a;
+        if(n<0){
+            a="number is negative";
+        }
+        else{
+            a="number is positive";
+        }
+        return a;
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int ch;
+        do{
+            int n= sc.nextInt();
+
+            System.out.println(neg_or_pos(n));
+            System.out.println("if you want to continue press 1");
+            ch=sc.nextInt();
+        }while (ch==1);
+    }
+}
