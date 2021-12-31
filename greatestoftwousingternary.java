8d7391ab723a1db63e19c66e6e4707e40c3b75c7
yashwanthi28 <71919596+yashwanthi28@users.noreply.github.com>
2021-12-31 17:26:05 +0530

    Add files via upload

---
+++ greatestoftwousingternary.java
@@ -0,0 +1,12 @@
+package project51;
+public class greatestoftwousingternary {
+	public static void main(String args[])
+	{
+		int n1 = 5, n2 = 10, max;
+		max = (n1 > n2) ? n1 : n2;
+	    System.out.println("Largest number between " + n1 +
+					" and " + n2 + " is " + max + ". " );
+	}
+	}
+
+
