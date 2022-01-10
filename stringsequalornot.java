d6919f56dd773f75b05f276384f651d59a9c6372
yashwanthi28 <71919596+yashwanthi28@users.noreply.github.com>
2022-01-10 17:16:41 +0530

    Add files via upload

---
+++ stringsequalornot.java
@@ -0,0 +1,20 @@
+package project77;
+import java.util.Scanner;
+public class stringsequalornot {
+	    public static void main(String args[]) {
+	        String str1, str2;
+	        Scanner scanner = new Scanner(System.in);
+	 
+	        System.out.println("Enter first String");
+	        str1 = scanner.nextLine();
+	 
+	        System.out.println("Enter second String");
+	        str2 = scanner.nextLine();
+	        // Comparing two input string
+	        if (str1.equals(str2))
+	            System.out.print("Equal Strings");
+	        else
+	            System.out.print("UnEqual Strings");
+	    }
+	}
+
