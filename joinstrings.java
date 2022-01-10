5332d87c6e307764c2f8ba76000d2917c36e53de
yashwanthi28 <71919596+yashwanthi28@users.noreply.github.com>
2022-01-10 17:15:40 +0530

    Add files via upload

---
+++ joinstrings.java
@@ -0,0 +1,13 @@
+package project76;
+import java.util.Scanner;
+public class joinstrings {
+	    public static void main(String[] args) {
+	        Scanner sc=new Scanner(System.in);
+	        System.out.println("enter first string");
+	        String a= sc.nextLine();
+	        System.out.println("enter second string");
+	        String b= sc.nextLine();
+	        System.out.println("after joining the strings");
+	        System.out.println(a+b);
+	    }
+}
