2f3878689112f9540e97bfd6b93e4075941ee4cd
yashwanthi28 <71919596+yashwanthi28@users.noreply.github.com>
2022-01-18 22:15:24 +0530

    Add files via upload

---
+++ size4matrix.java
@@ -0,0 +1,35 @@
+package project92;
+
+public class size4matrix {
+		public static void main(String[] args) {
+			int[][]A= {{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
+			int i,j;
+			int r=A.length;
+			int c=A[0].length;
+			for(i=0;i<r;i++) { 
+				for(j=i;j<c;j++) {
+					int temp=A[i][j];
+					A[i][j]=A[j][i];
+					A[j][i]=temp;
+				}
+			}
+			for( i=0;i<c;i++){
+			      int top=0;
+			      int bottom = c-1;
+			      while(top<bottom){
+			         int temp = A[top][i];
+			         A[top][i]=A[bottom][i];
+			         A[bottom][i] = temp;
+			         top++;
+			         bottom--;
+			      }
+			   }
+			for(i=0;i<r;i++) {
+				for(j=0;j<c;j++) {
+					System.out.print(A[i][j]+" ");
+				}
+			System.out.println();
+			}
+				}
+			}
+
